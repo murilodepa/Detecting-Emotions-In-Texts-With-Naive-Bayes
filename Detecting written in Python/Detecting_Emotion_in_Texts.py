68c14a7338e6ac657b9846559af53d4a5b9d3d8c
Murilo Araujo <murilodepa@gmail.com>
2020-02-15 15:28:33 -0300

    Adding dataset base

--- Detecting written in Python/Detecting_Emotion_in_Texts.py
+++ Detecting written in Python/Detecting_Emotion_in_Texts.py
@@ -2,18 +2,23 @@ import nltk
 
 #nltk.download()
 
-texto = "Hello Word. My name is Murilo. I am 21 years old."
-print(texto.split('.'))
-
-
-frase = nltk.tokenize.sent_tokenize(texto)
-print(frase)
-
-tokens = nltk.word_tokenize(texto)
-print(tokens)
-
-classes = nltk.pos_tag(tokens)
-print(classes)
-
-entidade = nltk.chunk.ne_chunk(classes)
-print(entidade)
+base = [('eu sou admirada por muitos','alegria'),
+        ('me sinto completamente amado','alegria'),
+        ('amar e maravilhoso','alegria'),
+        ('estou me sentindo muito animado novamente','alegria'),
+        ('eu estou muito bem hoje','alegria'),
+        ('que belo dia para dirigir um carro novo','alegria'),
+        ('o dia está muito bonito','alegria'),
+        ('estou contente com o resultado do teste que fiz no dia de ontem','alegria'),
+        ('o amor e lindo','alegria'),
+        ('nossa amizade e amor vai durar para sempre', 'alegria'),
+        ('estou amedrontado', 'medo'),
+        ('ele esta me ameacando a dias', 'medo'),
+        ('isso me deixa apavorada', 'medo'),
+        ('este lugar e apavorante', 'medo'),
+        ('se perdermos outro jogo seremos eliminados e isso me deixa com pavor', 'medo'),
+        ('tome cuidado com o lobisomem', 'medo'),
+        ('se eles descobrirem estamos encrencados', 'medo'),
+        ('estou tremendo de medo', 'medo'),
+        ('eu tenho muito medo dele', 'medo'),
+        ('estou com medo do resultado dos meus testes', 'medo')]
