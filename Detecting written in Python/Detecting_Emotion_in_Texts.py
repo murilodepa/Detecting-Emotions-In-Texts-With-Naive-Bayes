cf8408277d1f4a90bd58c0929cc8de9b582dd515
Murilo Araujo <murilodepa@gmail.com>
2020-02-15 00:16:30 -0300

    Learning to manipulate and utilize library NLTK

--- Detecting written in Python/Detecting_Emotion_in_Texts.py
+++ Detecting written in Python/Detecting_Emotion_in_Texts.py
@@ -1,1 +1,19 @@
-print('Hello World')
+import nltk
+
+#nltk.download()
+
+texto = "Hello Word. My name is Murilo. I am 21 years old."
+print(texto.split('.'))
+
+
+frase = nltk.tokenize.sent_tokenize(texto)
+print(frase)
+
+tokens = nltk.word_tokenize(texto)
+print(tokens)
+
+classes = nltk.pos_tag(tokens)
+print(classes)
+
+entidade = nltk.chunk.ne_chunk(classes)
+print(entidade)
